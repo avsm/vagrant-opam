1f6afc6b659502d2162cc3c9a35a892372f5736f
Anil Madhavapeddy <anil@recoil.org>
2014-01-15 14:25:37 +0000

    Add Aspcud to the Vagrant setup, for a superior solver experience

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,5 @@ apt-get update
 apt-get install -y python-software-properties build-essential m4
 add-apt-repository ppa:avsm/ppa
 apt-get update
-apt-get install -y ocaml ocaml-native-compilers camlp4 camlp4-extra opam git libssl-dev emacs vim nginx
+apt-get install -y ocaml ocaml-native-compilers camlp4 camlp4-extra opam git libssl-dev emacs vim nginx aspcud
 sed -i -e 's,/usr/share/nginx/html,/home/vagrant/.opam/doc/doc,g' /etc/nginx/sites-available/default
